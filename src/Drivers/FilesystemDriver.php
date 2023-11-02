7d8b978a747ed245ad088423ad74ad00e95eef85
Can Vural <can.vural@studocu.com>
2023-11-02 15:17:05 +0100

    feat: add filesystem driver

---
+++ src/Drivers/FilesystemDriver.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spatie\OpenTelemetry\Drivers;
+
+use Illuminate\Support\Facades\Storage;
+use Spatie\OpenTelemetry\Support\Formatters\SpanFormatter;
+
+class FilesystemDriver implements Driver
+{
+    /** @param array<string, mixed> $options */
+    public function __construct(private readonly SpanFormatter $spanFormatter, private readonly array $options)
+    {
+    }
+
+    /** @inheritDoc */
+    public function sendSpans(array $spans): void
+    {
+        $payload = $this->spanFormatter->format($spans);
+
+        Storage::disk($this->options['disk'] ?? 'local')->put($this->options['path'] ?? 'opentelemetry.json', $payload);
+    }
+}
